a10b2d4691241113cc79b259b6c405e50d4d02a7
Shalomking <favour912@gmail.com>
2022-06-24 22:49:36 +0200

    Javascript file

---
+++ script.js
@@ -0,0 +1,14 @@
+<script>
+    //function for displaying values function dis(val)
+    {
+        document.getElementById("cal").value+=val&nbsp;}
+        //function for evaluation function solve()
+        {
+            let x = document.getElementById("cal").value let y = eval(x)
+            document.getElementById("cal").value = y
+        }
+        //function for clearing the display function clr()
+        {
+            document.getElementById("cal").value = ''''
+        }
+</script>
